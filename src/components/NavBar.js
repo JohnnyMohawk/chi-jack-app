a26265917fe937b2c63d87d9bcbeb45100f5a698
JohnnyMohawk <johnnelsonalden@gmail.com>
2021-10-18 21:23:04 -0500

    add all tabs to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 export default function NavBar() {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
+
+    const closeMobileMenu = () => setClick(false)
     return (
         <>
             <nav className="navbar">
@@ -9,6 +14,36 @@ export default function NavBar() {
                     <Link to="/" className="navbar-logo">
                         Shy Jack <i className="fab fa-typo3" />
                     </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/map' className='nav-links' onClick={closeMobileMenu}>
+                                Map
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/data' className='nav-links' onClick={closeMobileMenu}>
+                                Data
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/scanner' className='nav-links' onClick={closeMobileMenu}>
+                                Scanner
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/forum' className='nav-links' onClick={closeMobileMenu}>
+                                Forum
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>
