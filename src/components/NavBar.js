62840df9f043b6b29818de87f09d50015097f19c
JohnnyMohawk <johnnelsonalden@gmail.com>
2021-10-18 22:39:02 -0500

    add car icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -30,7 +30,7 @@ function Navbar() {
             <div className='navbar-container'>
             <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                 Shy Jack
-                <i class='fab fa-typo3' />
+                <i class="fas fa-car-crash"></i>
             </Link>
             <div className='menu-icon' onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
