0814c342086dbc9ebf3ad00fbe19e20407f2d73f
JohnnyMohawk <johnnelsonalden@gmail.com>
2021-10-20 13:34:01 -0500

    add Map.css

--- src/components/pages/Map.js
+++ src/components/pages/Map.js
@@ -1,9 +1,18 @@
 import React from "react";
 import '../../App.css'
+import '../pages/Map.css'
 
 function Map() {
     return (
-        <h1 className='map'>Map Party</h1>
+        <div className="map-container">
+            {/* <video src="/videos/tulips.mp4" autoPlay loop muted /> */}
+            
+            <h1>Interactive Chicago Carjacking Map</h1>
+            
+            <div className="map-text">
+                <p>Ad id ad cupidatat dolore magna nulla et tempor est laborum tempor sunt. Cillum irure duis labore tempor reprehenderit. Cillum eu excepteur fugiat exercitation commodo anim minim consectetur ex enim id duis enim. Sit sint officia irure adipisicing officia esse cillum. Anim veniam exercitation duis dolor irure dolore esse aliqua dolore laborum veniam dolore ex. Occaecat eu reprehenderit voluptate dolore et nostrud labore et dolor aliquip. Non ut deserunt aliquip qui cillum excepteur ullamco aliqua qui laborum.</p>
+            </div>
+        </div>
     )
 
 }
