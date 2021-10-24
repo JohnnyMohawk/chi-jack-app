17c22a0bca1c3107f088dfffe57f9927eeb630dc
JohnnyMohawk <johnnelsonalden@gmail.com>
2021-10-24 12:45:03 -0500

    add landing page transition in animation

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -13,7 +13,7 @@ function HeroSection() {
                 <i class="fas fa-car-crash"></i>
             </Link>
             <h2>City of Chicago Carjacking Data Visualizer</h2>
-            <p>Just Data <i class="fas fa-database"></i> No Spin</p>
+            <p className="slogan">Just Data <i class="fas fa-database"></i> No Spin</p>
             <div className="hero-btns">
                 <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large" to="/map">Map Data</Button>
                 <Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large" to="/graph">Graph Data</Button>
