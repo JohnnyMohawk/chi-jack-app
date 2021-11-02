c161be32c49f35d423335aae91393f5ea810d213
JohnnyMohawk <johnnelsonalden@gmail.com>
2021-11-02 09:46:27 -0500

    improve button styling and mobile view

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,7 +53,7 @@ function Footer(props) {
                             id="email"
                             value={formData.email}
                             onChange={handleChange} />
-                        <Button to="/sign-up" onClick={handleSubmit} buttonStyle='btn--outline' buttonSize="btn--large">Subscribe</Button>
+                        <Button to="/sign-up" onClick={handleSubmit} buttonStyle='btn--outline' buttonSize="btn--medium">Subscribe</Button>
                     </form>
                 </div>
             </section>
