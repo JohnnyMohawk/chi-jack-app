9e2baa6a29edf7e79f28d85ae6d08fabb88d9b08
JohnnyMohawk <johnnelsonalden@gmail.com>
2022-06-13 23:16:05 -0500

    move news api call server side

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,72 +11,19 @@ function Cards() {
     const [pages, setPages] = useState(0)
     const [counter, setCounter] = useState(0)
 
-    // const yesterday = new Date('2022-06-30').toISOString().split('T')[0]
-
-    const makeNewsApiCall = async() => {
-        let pullDate = await getPullDate()
-        setNumPages(pullDate.numPages)
-        setPages(pullDate.pages)
-        try {
-            if(new Date().toISOString().split('T')[0] > new Date(pullDate.pullDate).toISOString().split('T')[0]){
-                let res = await fetch('https://newsapi.org/v2/everything?q=chicago+carjacking&sortBy=publishedAt&domains=wgntv.com,abc7chicago.com,foxnews.com,nbcnews.com,nypost.com,chicagotribune.com,abcnews.go.com,chicago.suntimes.com,wbez.org,thedailybeast.com,dailycaller.com,nypost.com&apiKey=fca7629171c143338ccaa74f5c0bb383')
-                const newsData = await res.json()
-                console.log("Inner makeNewsApiCall Log", newsData)
-                setNews(newsData)
-                if(newsData.status === "ok"){
-                    newsService.todaysNews({status: newsData.status, totalResults: newsData.totalResults, articles: newsData.articles})
-                }
-            }else{
-                getNews()
-            }
-        } catch (error) {
-            throw error
-        }
-    }
-
-    const getNews = async() => {
-        let res = await fetch('api/news')
-        const newsDbData = await res.json()
-        console.log("Inner getNews Log", newsDbData)
-        setNews(newsDbData)
-    }
-
-    const getPullDate = async() => {
-        let res = await fetch('api/news')
-        const newsDbData = await res.json()
-        return (
-            {
-                pullDate: new Date(newsDbData.updatedAt.split('T')[0]),
-                numPages: Math.floor(newsDbData.articles.length / 5),
-                pages: chunkArray(indexArray(newsDbData.articles), 5)
-            }
-        )
-    }
-
-    const indexArray = (array) => {
-        let iArray = []
-        for (let index = 0; index < array.length; index++) {
-            iArray.push(index)
-        }
-        return iArray
-    }
-
-    const chunkArray = (myArray, chunk_size) => {
-        var index = 0;
-        var arrayLength = myArray.length;
-        var tempArray = [];
-        for (index = 0; index < arrayLength; index += chunk_size) {
-            let myChunk = myArray.slice(index, index + chunk_size);
-            tempArray.push(myChunk);
-        }
-        return tempArray;
+    const serverSideApiCall = async() => {
+        let newsObj = await newsService.makeNewsApiCall()
+        setNumPages(newsObj.numPages)
+        setPages(newsObj.pages)
+        setNews(newsObj.news)
     }
 
     useEffect(() => {
-        makeNewsApiCall()
+        serverSideApiCall()
     }, [])
 
     return (
+        
         news ? 
         <>
         <div className='cards'>
@@ -87,13 +34,13 @@ function Cards() {
                         <CardItem 
                         src={news.articles[pages[counter][0]].urlToImage}
                             text={news.articles[pages[counter][0]].content}
-                            label={`${news.articles[pages[counter][0]].source.name} ${new Date(news.articles[0].publishedAt.split('T')[0]).toDateString()}`}
+                            label={`${news.articles[pages[counter][0]].source.name} ${new Date(news.articles[pages[counter][0]].publishedAt).toDateString()}`}
                             path={news.articles[pages[counter][0]].url}
                         />
                         <CardItem 
                             src={news.articles[pages[counter][1]].urlToImage}
                             text={news.articles[pages[counter][1]].content}
-                            label={`${news.articles[pages[counter][1]].source.name} ${new Date(news.articles[1].publishedAt.split('T')[0]).toDateString()}`}
+                            label={`${news.articles[pages[counter][1]].source.name} ${new Date(news.articles[pages[counter][1]].publishedAt).toDateString()}`}
                             path={news.articles[pages[counter][1]].url}
                         />
                     </ul>
@@ -101,19 +48,19 @@ function Cards() {
                         <CardItem 
                             src={news.articles[pages[counter][2]].urlToImage}
                             text={news.articles[pages[counter][2]].content}
-                            label={`${news.articles[pages[counter][2]].source.name} ${new Date(news.articles[2].publishedAt.split('T')[0]).toDateString()}`}
+                            label={`${news.articles[pages[counter][2]].source.name} ${new Date(news.articles[pages[counter][2]].publishedAt).toDateString()}`}
                             path={news.articles[pages[counter][2]].url}
                         />
                         <CardItem 
                             src={news.articles[pages[counter][3]].urlToImage}
                             text={news.articles[pages[counter][3]].content}
-                            label={`${news.articles[pages[counter][3]].source.name} ${new Date(news.articles[3].publishedAt.split('T')[0]).toDateString()}`}
+                            label={`${news.articles[pages[counter][3]].source.name} ${new Date(news.articles[pages[counter][3]].publishedAt).toDateString()}`}
                             path={news.articles[pages[counter][3]].url}
                         />
                         <CardItem 
                             src={news.articles[pages[counter][4]].urlToImage}
                             text={news.articles[pages[counter][4]].content}
-                            label={`${news.articles[pages[counter][4]].source.name} ${new Date(news.articles[4].publishedAt.split('T')[0]).toDateString()}`}
+                            label={`${news.articles[pages[counter][4]].source.name} ${new Date(news.articles[pages[counter][4]].publishedAt).toDateString()}`}
                             path={news.articles[pages[counter][4]].url}
                         />
                     </ul>
--- src/services/newsService.js
+++ src/services/newsService.js
@@ -1,5 +1,3 @@
-// import { News } from "../../models/news"
-
 const BASE_URL = '/api/news/'
 
 
@@ -17,12 +15,77 @@ export const todaysNews = async (news) => {
     }
 }
 
-// export const getNews = async () => {
+// export const makeNewsApiCall = async() => {
 //     try {
-//         const res = await fetch(BASE_URL, { mode: "cors" })
-//         const data = await res.json()
-//         return data
+//         let res = await fetch('https://newsapi.org/v2/everything?q=chicago+carjacking&sortBy=publishedAt&domains=wgntv.com,abc7chicago.com,foxnews.com,nbcnews.com,nypost.com,chicagotribune.com,abcnews.go.com,chicago.suntimes.com,wbez.org,thedailybeast.com,dailycaller.com,nypost.com&apiKey=fca7629171c143338ccaa74f5c0bb383')
+//         const newsData = await res.json()
+//         console.log("Inner makeNewsApiCall Log", newsData)
+//         return (
+//             {
+//                 news: newsData,
+//                 numPages: Math.floor(newsData.articles.length / 5),
+//                 pages: chunkArray(indexArray(newsData.articles), 5),
+//             }
+//         )
 //     } catch (error) {
 //         throw error
 //     }
 // }
+
+export const makeNewsApiCall = async() => {
+    let dbNews = await getNews()
+    try {
+        if(new Date().toISOString().split('T')[0] > new Date(dbNews.pullDate).toISOString().split('T')[0]){
+        let res = await fetch('https://newsapi.org/v2/everything?q=chicago+carjacking&sortBy=publishedAt&domains=wgntv.com,abc7chicago.com,foxnews.com,nbcnews.com,nypost.com,chicagotribune.com,abcnews.go.com,chicago.suntimes.com,wbez.org,thedailybeast.com,dailycaller.com,nypost.com&apiKey=fca7629171c143338ccaa74f5c0bb383')
+        const newsData = await res.json()
+        console.log("Inner makeNewsApiCall Log", newsData)
+        if(newsData.status === "ok"){
+            todaysNews({status: newsData.status, totalResults: newsData.totalResults, articles: newsData.articles})
+        }
+        return (
+            {
+                news: newsData,
+                numPages: Math.floor(newsData.articles.length / 5),
+                pages: chunkArray(indexArray(newsData.articles), 5),
+            }
+        )
+        }else{
+            return dbNews
+        }
+    } catch (error) {
+        throw error
+    }
+}
+
+export const getNews = async() => {
+    let res = await fetch('api/news')
+    const newsDbData = await res.json()
+    console.log("Inner getNews Log", newsDbData)
+    return (
+        {
+            news: newsDbData,
+            numPages: Math.floor(newsDbData.articles.length / 5),
+            pages: chunkArray(indexArray(newsDbData.articles), 5),
+            pullDate: new Date(newsDbData.updatedAt.split('T')[0]),
+        }
+    )
+}
+
+export const indexArray = (array) => {
+    let iArray = []
+    for (let index = 0; index < array.length; index++) {
+        iArray.push(index)
+    }
+    return iArray
+}
+
+export const chunkArray = (myArray, chunk_size) => {
+    var index = 0;
+    var arrayLength = myArray.length;
+    var tempArray = [];
+    for (index = 0; index < arrayLength; index += chunk_size) {
+        let myChunk = myArray.slice(index, index + chunk_size);
+        tempArray.push(myChunk);
+    }
+    return tempArray;
+}
