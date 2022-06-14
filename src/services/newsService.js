f73757e29842c4f17b1082452b0b4354c09df46f
JohnnyMohawk <johnnelsonalden@gmail.com>
2022-06-13 22:33:02 -0500

    move news api call server side

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -1,5 +1,3 @@
-// import { News } from "../../models/news"
-
 const BASE_URL = '/api/news/'
 
 
@@ -15,6 +13,63 @@ export const todaysNews = async (news) => {
     } catch (error) {
         throw error
     }
+}
+
+
+export const makeNewsApiCall = async() => {
+    try {
+        let res = await fetch('https://newsapi.org/v2/everything?q=chicago+carjacking&sortBy=publishedAt&domains=wgntv.com,abc7chicago.com,foxnews.com,nbcnews.com,nypost.com,chicagotribune.com,abcnews.go.com,chicago.suntimes.com,wbez.org,thedailybeast.com,dailycaller.com,nypost.com&apiKey=fca7629171c143338ccaa74f5c0bb383')
+        const newsData = await res.json()
+        console.log("Inner makeNewsApiCall Log", newsData)
+        return (
+            {
+                news: newsData,
+                numPages: Math.floor(newsData.articles.length / 5),
+                pages: chunkArray(indexArray(newsData.articles), 5),
+            }
+        )
+    } catch (error) {
+        throw error
+    }
+}
+
+// export const getNews = async() => {
+//     let res = await fetch('api/news')
+//     const newsDbData = await res.json()
+//     console.log("Inner getNews Log", newsDbData)
+//     return newsDbData
+// }
+
+// export const getPullDate = async() => {
+//     let res = await fetch('api/news')
+//     const newsDbData = await res.json()
+//     return (
+//         {
+//             pullDate: new Date(newsDbData.updatedAt.split('T')[0]),
+//             numPages: Math.floor(newsDbData.articles.length / 5),
+//             pages: chunkArray(indexArray(newsDbData.articles), 5),
+//             articles: newsDbData.articles,
+//         }
+//     )
+// }
+
+export const indexArray = (array) => {
+    let iArray = []
+    for (let index = 0; index < array.length; index++) {
+        iArray.push(index)
+    }
+    return iArray
+}
+
+export const chunkArray = (myArray, chunk_size) => {
+    var index = 0;
+    var arrayLength = myArray.length;
+    var tempArray = [];
+    for (index = 0; index < arrayLength; index += chunk_size) {
+        let myChunk = myArray.slice(index, index + chunk_size);
+        tempArray.push(myChunk);
+    }
+    return tempArray;
 }
 
 // export const getNews = async () => {
