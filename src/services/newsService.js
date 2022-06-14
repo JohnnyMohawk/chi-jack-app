4b95aef6c30f3be15bc2750e8e8c69debd892751
JohnnyMohawk <johnnelsonalden@gmail.com>
2022-06-13 23:06:22 -0500

    serverside api call now saves to db so api call only made once a day at most

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -15,12 +15,33 @@ export const todaysNews = async (news) => {
     }
 }
 
+// export const makeNewsApiCall = async() => {
+//     try {
+//         let res = await fetch('https://newsapi.org/v2/everything?q=chicago+carjacking&sortBy=publishedAt&domains=wgntv.com,abc7chicago.com,foxnews.com,nbcnews.com,nypost.com,chicagotribune.com,abcnews.go.com,chicago.suntimes.com,wbez.org,thedailybeast.com,dailycaller.com,nypost.com&apiKey=fca7629171c143338ccaa74f5c0bb383')
+//         const newsData = await res.json()
+//         console.log("Inner makeNewsApiCall Log", newsData)
+//         return (
+//             {
+//                 news: newsData,
+//                 numPages: Math.floor(newsData.articles.length / 5),
+//                 pages: chunkArray(indexArray(newsData.articles), 5),
+//             }
+//         )
+//     } catch (error) {
+//         throw error
+//     }
+// }
 
 export const makeNewsApiCall = async() => {
+    let dbNews = await getNews()
     try {
+        if(new Date().toISOString().split('T')[0] > new Date(dbNews.pullDate).toISOString().split('T')[0]){
         let res = await fetch('https://newsapi.org/v2/everything?q=chicago+carjacking&sortBy=publishedAt&domains=wgntv.com,abc7chicago.com,foxnews.com,nbcnews.com,nypost.com,chicagotribune.com,abcnews.go.com,chicago.suntimes.com,wbez.org,thedailybeast.com,dailycaller.com,nypost.com&apiKey=fca7629171c143338ccaa74f5c0bb383')
         const newsData = await res.json()
         console.log("Inner makeNewsApiCall Log", newsData)
+        if(newsData.status === "ok"){
+            todaysNews({status: newsData.status, totalResults: newsData.totalResults, articles: newsData.articles})
+        }
         return (
             {
                 news: newsData,
@@ -28,30 +49,27 @@ export const makeNewsApiCall = async() => {
                 pages: chunkArray(indexArray(newsData.articles), 5),
             }
         )
+        }else{
+            return dbNews
+        }
     } catch (error) {
         throw error
     }
 }
 
-// export const getNews = async() => {
-//     let res = await fetch('api/news')
-//     const newsDbData = await res.json()
-//     console.log("Inner getNews Log", newsDbData)
-//     return newsDbData
-// }
-
-// export const getPullDate = async() => {
-//     let res = await fetch('api/news')
-//     const newsDbData = await res.json()
-//     return (
-//         {
-//             pullDate: new Date(newsDbData.updatedAt.split('T')[0]),
-//             numPages: Math.floor(newsDbData.articles.length / 5),
-//             pages: chunkArray(indexArray(newsDbData.articles), 5),
-//             articles: newsDbData.articles,
-//         }
-//     )
-// }
+export const getNews = async() => {
+    let res = await fetch('api/news')
+    const newsDbData = await res.json()
+    console.log("Inner getNews Log", newsDbData)
+    return (
+        {
+            news: newsDbData,
+            numPages: Math.floor(newsDbData.articles.length / 5),
+            pages: chunkArray(indexArray(newsDbData.articles), 5),
+            pullDate: new Date(newsDbData.updatedAt.split('T')[0]),
+        }
+    )
+}
 
 export const indexArray = (array) => {
     let iArray = []
@@ -71,13 +89,3 @@ export const chunkArray = (myArray, chunk_size) => {
     }
     return tempArray;
 }
-
-// export const getNews = async () => {
-//     try {
-//         const res = await fetch(BASE_URL, { mode: "cors" })
-//         const data = await res.json()
-//         return data
-//     } catch (error) {
-//         throw error
-//     }
-// }
